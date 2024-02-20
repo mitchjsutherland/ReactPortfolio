b796d05107c84111fa061ef7bbeab7cbb60968a6
Mitch Sutherland <mitchelsutherland@icloud.com>
2024-02-20 17:50:07 +0000

    Project page update

--- msreactportfolio/src/Homepage.jsx
+++ msreactportfolio/src/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PortfolioProfile from './assets/ProfilePic2.png'
+import { Link } from 'react-router-dom';
 
 function Homepage() {
 
@@ -9,13 +10,13 @@ function Homepage() {
 
                 <div className="row align-items-center justify-content-center text-center" id="new-mainbody">
                     <div className="col-sm-7 col-md-2 col-lg-2" id="new-mainprojects">
-                        <h4 className= "home-action">PROJECTS</h4>
+                        <Link to = "/projects"><h4 className= "home-action">PROJECTS</h4></Link>
                     </div>
                     <div className="col-sm-11 col-md-6" id="new-mainprofile">
                         <img src={PortfolioProfile} className="profile-pic" id="profile-picid"></img>
                     </div>
                     <div className="col-sm-7 col-md-2 col-lg-2" id="new-maincontact">
-                        <h4 className= "home-action">CONTACT</h4>
+                        <Link to="/contact"><h4 className= "home-action">CONTACT</h4></Link>
                     </div>
                 </div>
 
--- msreactportfolio/src/Projects.jsx
+++ msreactportfolio/src/Projects.jsx
@@ -6,11 +6,20 @@ function Projects() {
         <>
             <main className="mainprojects">
 
-                <div className="container">
-                    <div className="col-6">
-                        <div className="div title-wrapper">
-                            <h1>Projects</h1>
-                            <h1>Portfolio</h1>
+                <div className="container projectbody">
+                    <div className="col-6 project-title-wrapper">
+                        <div className = "col-12">
+                            <div className="div projects-title" id="projects-title">
+                                <h1 id="p-title-1">Projects</h1>
+                                <h1 id="p-title-2">Portfolio</h1>
+                            </div>
+                            <div className="div projects-subtitle" id="projects-subtitle">
+                                <h5>Because the proof is in the projects, right?</h5>
+                            </div>
+                        </div>
+                        <div className = "project-info">
+                            <h2>Project Name</h2>
+                            <h3>Project Description</h3>
                         </div>
                     </div>
                     <div className="col-6">
